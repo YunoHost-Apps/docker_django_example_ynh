10f153a8e44933c35a317f218a9f970e75e588ea
Jens Diemer <git@jensdiemer.de>
2023-12-31 18:02:09 +0100

    CURRENT_UID->USERMAP_UID + CURRENT_GID->USERMAP_GID

--- conf/compose.sh
+++ conf/compose.sh
@@ -5,11 +5,11 @@ source common.env
 set -ex
 
 # To run services as app user: Store UID/GID and use it in docker-compose.yml:
-CURRENT_UID="$(id -u)"
-export CURRENT_UID
+USERMAP_UID="$(id -u)"
+export USERMAP_UID
 
-CURRENT_GID="$(id -g)"
-export CURRENT_GID
+USERMAP_GID="$(id -g)"
+export USERMAP_GID
 
 # Compose v2 is always accessible as docker compose!
 # But Debian bullseye contains v1
